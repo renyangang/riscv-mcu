c8672074a1644df90cfc369e0da2addbfcbac312
ryg <ryg221@126.com>
2024-08-15 17:47:42 +0800

    boot

--- src/boot.s
+++ src/boot.s
@@ -2,24 +2,44 @@
 _start:
     # cust flag set instruction code
     .insn 4, 0x8000007F
+    # close interrupts
+    csrrw t0,mstatus,zero
     # ssd base address
-    lui x1, 0x1000
+    lui t1, 0x40000
     # offset
-    mv x2, x0
-    # total 512k bytes
-    lui x3, 0x80
+    mv t2, zero
+    # total 1024 bytes
+    addi t3, zero, 0x400
 loop:
-    lbu x4, 0(x1)
-    add x1,x1,x2
-    sb x4, 0(x2)
-    addi x2, x2, 1
-    blt x2, x3, loop
+    lbu t4, 0(t1)
+    add t1,t1,t2
+    sb t4, 0(t2)
+    addi t2, t2, 1
+    blt t2, t3, loop
     
+    # set boot status
+    li t1, 0xC0000100
+    csrrw t2,misa, t1
+    csrrw t2,mvendorid,zero
+    csrrw t2,marchid,zero
+    csrrw t2,mimpid,zero
+    csrrw t2,mhartid,zero
+    li t1,0x1808
+    csrrw t2,mstatus,t1
+    csrrw t2,mtvec,zero
+    csrrw t2,mepc,zero
+    csrrw t2,mcause,zero
+    csrrw t2,mtval,zero
+    csrrw t2,mip,zero
+    li t1,0x448
+    csrrw t2,mie,t1
+
     # clear all registers
-    li x1, 0
-    li x2, 0
-    li x3, 0
-    li x4, 0
+    li t0, 0
+    li t1, 0
+    li t2, 0
+    li t3, 0
+    li t4, 0
 
 main:
     j main
