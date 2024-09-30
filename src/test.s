b20fd95c12e641d28fa9d715132555508e482e37
renyangang <ryg221@126.com>
2024-09-30 21:02:43 +0800

    pipeline hazards

--- src/test.s
+++ src/test.s
@@ -3,5 +3,25 @@
 _start:
     li x1, 0x1
     li x2, 0x2
+b0:
     add x3, x1, x2
     li x4, 0x3
+    # bne x3, x0, b0
+    # beq x3, x0, b0
+    # beq x3, x0, b1
+    # bne x3, x0, b1
+    # j b1
+    lw x9, 68(x0)
+    addi x10, x9, 0x1
+    li x5, 0x5
+    li x6, 0x6
+    li x7, 0x7
+b1:
+    li x8, 0x8
+
+d0:
+    .rept 10
+    .long 0x1
+    .endr
+
+.end
